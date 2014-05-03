177d24962126fabd9af6a5fded2f2de3d8cf4b7b
chris <christopher.gardn@gmail.com>
2014-05-03 00:58:46 -0400

    made reusable

--- p3.py
+++ p3.py
@@ -4,30 +4,32 @@
 import sys
 import numpy as np
 
-n = int(sys.argv[1])
 
-f = 2
-
-if n % 2 == 0:
-    n = n/2
-    lf = 2
-    while n % 2 == 0:
-        n = n/2
+def factor(n):
+    f = 2
+    lf = 1
+    if n % 2 == 0:
+        lf = 2
+        while n % 2 == 0:
+            n = n/2
     else:
         lf = 1
 
-f=3
-mf = np.sqrt(n)
-while n>1 & f <= mf:
-    if n % f == 0:
-        n = n/f
-        lf = f
-        while n % f == 0:
+    f = 3
+    mf = np.sqrt(n)
+    while n>1 & f <= mf:
+        if n % f == 0:
             n = n/f
-        mf=np.sqrt(n)
-    f += 2
+            lf = f
+            while n % f == 0:
+                n = n/f
+            mf=np.sqrt(n)
+        f += 2
 
-if n == 1:
-    print lf
-else:
-    print n
+    if n == 1:
+        return lf
+    else:
+        return n
+
+if __name__ == '__main__':
+    print factor(int(sys.argv[1]))
